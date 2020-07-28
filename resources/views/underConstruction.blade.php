2a18f55fedad4698dba106cb62a6f0d357385164
mrsbaai <abdelilah.sbaai@gmail.com>
2020-07-28 21:41:04 +0100

    helloworld

---
+++ resources/views/underConstruction.blade.php
@@ -0,0 +1,27 @@
+<html>
+  <head>
+    <title>Code Camp: Coming Soon</title>
+	
+    <style>
+	body {
+		background-color: #ff1616;
+	}
+    img {
+	   position: absolute;
+	   top: 50%;
+	   left: 50%;
+	   width: 500px;
+	   height: 500px;
+	   margin-top: -250px; /* Half the height */
+	   margin-left: -250px; /* Half the width */
+	}
+</style>
+
+    </style>
+  </head>
+  <body>
+    <div class="img-container">
+      <img src="https://i.imgur.com/BrC6hTR.png" alt="Code Camp">
+    </div>
+  </body>
+</html>
