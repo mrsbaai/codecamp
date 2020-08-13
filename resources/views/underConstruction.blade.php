f50fdc8305c44c3507f1d912c7481d02c5a39557
mrsbaai <abdelilah.sbaai@gmail.com>
2020-08-13 18:16:04 +0100

    fix

--- resources/views/underConstruction.blade.php
+++ resources/views/underConstruction.blade.php
@@ -174,9 +174,9 @@
                 <div class="validate"></div>
               </div>
               <div class="mb-3">
-                <div class="loading">loading</div>
-                <div class="error-message"></div>
-                <div class="sent-message">thanks</div>
+                <div class="loading">Loading...</div>
+                <div class="error-message">An error has occurred :(</div>
+                <div class="sent-message">Message Received :)</div>
               </div>
               <div class="text-center"><button type="submit">Send Message</button></div>
             {{ Form::close() }}
