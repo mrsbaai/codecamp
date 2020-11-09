842c5950edb7aafbc7c6085916ba253d8b601298
mrsbaai <abdelilah.sbaai@gmail.com>
2020-11-09 16:28:16 +0100

    Update underConstruction.blade.php

--- resources/views/underConstruction.blade.php
+++ resources/views/underConstruction.blade.php
@@ -66,7 +66,7 @@
         <a href="https://linkedin.com/company/code-camp-ma" class="linkedin"><i class="bx bxl-linkedin"></i></a>
       </div>
       <br/>
-      <a href="/#services" class="linkedin"><i class="bx bxl-down-arrow-circle"></i></a>
+      <a href="/#services" style="font-size: 400%"><i class="bx bx-down-arrow-circle"></i></a>
 
     </div>
   </section><!-- End Hero -->
