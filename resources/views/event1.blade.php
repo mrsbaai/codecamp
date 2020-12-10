8e2811e9a25cf6ca70b36b0a471fff2e68babbc8
mrsbaai <abdelilah.sbaai@gmail.com>
2020-12-10 13:37:14 +0100

    Update event1.blade.php

--- resources/views/event1.blade.php
+++ resources/views/event1.blade.php
@@ -81,7 +81,7 @@
 
     <div class="container" data-aos="zoom-in" data-aos-delay="100">
 	
-      <p><span class="typed" data-typed-items="Code Camp Events: Social Engineering [Ethical Hacking]"></span>
+      <p><span class="typed" data-typed-items="Social Engineering [Ethical Hacking] [Code Camp Events]"></span>
         <br/>
         <blockquote class="blockquote">
           <p class="mb-0">
