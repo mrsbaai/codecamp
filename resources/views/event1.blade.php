85624c1ce830608c549234a431af12cab95addec
mrsbaai <abdelilah.sbaai@gmail.com>
2020-12-10 12:29:45 +0100

    up

--- resources/views/event1.blade.php
+++ resources/views/event1.blade.php
@@ -53,7 +53,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
   <div class="container">
     <a class="navbar-brand" href="#">
-          <img src="https://codecamp.ma/construction/img/logo-big.png" alt="">
+          <img src="https://codecamp.ma/construction/img/logo-small.png" alt="">
         </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
