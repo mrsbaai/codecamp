bc2e0a87f876828407914ab437e9557c1940378c
mrsbaai <abdelilah.sbaai@gmail.com>
2020-12-27 16:49:30 +0100

    Update newyear.blade.php

--- resources/views/newyear.blade.php
+++ resources/views/newyear.blade.php
@@ -140,11 +140,7 @@
             </div>
           </div>
           <div class="form-group">
-            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-            <div class="validate"></div>
-          </div>
-          <div class="form-group">
-            <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+            <textarea class="form-control" name="business" rows="5" data-rule="required" data-msg="A short description of your business:" placeholder="A short description of your business"></textarea>
             <div class="validate"></div>
           </div>
           <div class="mb-3">
