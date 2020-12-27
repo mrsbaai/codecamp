bea0eb10cddab029b122332b1f72457a5f9bddb4
mrsbaai <abdelilah.sbaai@gmail.com>
2020-12-27 14:42:44 +0100

    Update coworking.blade.php

--- resources/views/coworking.blade.php
+++ resources/views/coworking.blade.php
@@ -5,8 +5,8 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Coming Soon</title>
-  <meta content="A social network, market place, learning camp, for independent internet builders." name="descriptison">
+  <title>CodeCamp Event: Social Engineering [Ethical Hacking]</title>
+  <meta content="CodeCamp Event About Social Engineering [Ethical Hacking]" name="descriptison">
 
 
   <!-- Favicons -->
@@ -49,163 +49,105 @@
 <body>
 
 
-
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+  <div class="container">
+    <a class="navbar-brand" href="https://codecamp.ma">
+          <img src="https://codecamp.ma/construction/img/logo-very-small.png" alt="">
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="https://codecamp.ma">Home
+                <span class="sr-only">(current)</span>
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#footer">Media</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://codecamp.ma/#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex flex-column justify-content-center">
 
     <div class="container" data-aos="zoom-in" data-aos-delay="100">
-	
-      <img src="/construction/img/logo-big.png" alt="Code Camp">
-      <p><span class="typed" data-typed-items="A Social Network, A Market Place, A Learning Camp"></span><br/>For Independent Internet Builders.</p>
-	
-      <div class="social-links">
-        <a href="https://www.youtube.com/channel/UCjWT7ulRfJ4W0MjnGF4lyDg" class="youtube"><i class="bx bxl-youtube"></i></a>
-        <a href="https://www.facebook.com/CodeCamp.ma" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="https://instagram/codecamp.ma" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="https://linkedin.com/company/code-camp-ma" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-      </div>
+  
+      <p><span class="typed" data-typed-items="Social Engineering [Ethical Hacking] [Code Camp Events]"></span>
+        <br/>
+        <blockquote class="blockquote">
+          <p class="mb-0">
+            Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker.
+          </p>
+          <footer class="blockquote-footer" style="color:white;">
+            Emmanuel Goldstein, Dear Hacker: Letters to the Editor of 2600
+          </footer>
+        </blockquote>
+      </p>
+
+
+
     </div>
   </section><!-- End Hero -->
 
-  <main id="main">
-
-
-
-    <!-- ======= Services Section ======= -->
-    <section id="services" class="services">
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-title">
-          <h2>We are:</h2>
-          
-        </div>
-
-        <div class="row">
-
-          <div class="col-lg-4 col-md-4 align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-            <div class="icon-box">
-              <h2>A Social Network</h2>
-              <p>Get Connected with other Independent Internet Builders.</p>
-            </div>
-          </div>
-		  
-		  <div class="col-lg-4 col-md-4 align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-            <div class="icon-box">
-              <h2>A Market Place</h2>
-              <p>Sell your stuff in our market place (It's free forever).</p>
-            </div>
-          </div>
-
-
-
-          <div class="col-lg-4 col-md-4 align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-            <div class="icon-box">
-              <h2>A Learning Place</h2>
-              <p>Exlusive content. Improve your knowlege and skills as an independent internet warrior.</p>
-            </div>
-          </div>
-
-
-
-
- 
-
-        </div>
-
-      </div>
-    </section><!-- End Services Section -->
-
-   
-
-    <!-- ======= Contact Section ======= -->
-    <section id="contact" class="contact">
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-title">
-          <h2>More info?</h2>
-        </div>
-
-        <div class="row mt-1">
-
-          <div class="col-lg-4">
-            <div class="info">
-              <div class="address">
-                <h4>Location:</h4>
-                <p>Wilaya Center N°55 ET 5 Tetouan Morocco.</p>
-              </div>
-
-              <div class="email">
-                <h4>Email:</h4>
-                <p>info@codecamp.ma</p>
-              </div>
-
-              <div class="phone">
-                <h4>Call:</h4>
-                <p>+212 700 12 41 56</p>
-              </div>
-
-            </div>
-
-          </div>
-
-          <div class="col-lg-8 mt-5 mt-lg-0">
-            {{ Form::open(array('action' => 'pagesController@saveForm', 'class' => 'php-email-form'))}}
-              <div class="form-row">
-                <div class="col-md-4 form-group">
-                  <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                  <div class="validate"></div>
-                </div>
-                <div class="col-md-4 form-group">
-                  <input type="text" name="phone" class="form-control" id="phone" placeholder="Your Phone Number" data-rule="minlen:8" data-msg="Please enter your realphone number" />
-                  <div class="validate"></div>
-                </div>
-                <div class="col-md-4 form-group">
-                  <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                  <div class="validate"></div>
-                </div>
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                <div class="validate"></div>
-              </div>
-              <div class="form-group">
-                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                <div class="validate"></div>
-              </div>
-              <div class="mb-3">
-                <div class="loading">Loading...</div>
-                <div class="error-message">An error has occurred :(</div>
-                <div class="sent-message">Message Received :)</div>
-              </div>
-              <div class="text-center"><button type="submit">Send Message</button></div>
-            {{ Form::close() }}
-
-          </div>
-
-        </div>
-
-      </div>
-    </section><!-- End Contact Section -->
-
-  </main><!-- End #main -->
-
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
+  <section id="contact" class="contact" style="color:white;background-color:#343A40;">
     <div class="container" data-aos="fade-up">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="page-header">
+
+          </div>
+
+          <p class="lead">
+            Social engineering is a technique hackers use to manipulate end users and obtain information about an organization or computer systems. In order to protect their networks, IT security professionals need to understand social engineering, who is targeted, and how social engineering attacks are orchestrated.<br>
+          </p>
       
-      <div class="social-links">
-        <a href="https://www.youtube.com/channel/UCjWT7ulRfJ4W0MjnGF4lyDg" class="youtube"><i class="bx bxl-youtube"></i></a>
-        <a href="https://www.facebook.com/CodeCamp.ma" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="https://instagram/codecamp.ma" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="https://linkedin.com/company/code-camp-ma" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+          <p class="lead">
+            In this course, cybersecurity expert Lisa Bock discusses the methods a hacker might use, including embedding malicious links and attachments in emails and using mobile devices and social media to deploy an attack. She discusses the concept of "misuse of trust"—how hackers use charm, power, and influence to penetrate an organization—and why you need to be extra cautious with the disgruntled employee. Finally, Lisa discusses countermeasures security professionals can take to address these attacks.<br>
+          </p>
+          <br/><br/>
+          BLA BLA..
+          
+          
+
+
+
+        
+        </div>
       </div>
-      <div class="copyright">
-        &copy; Copyright <strong><span>Code Camp</span></strong>. All Rights Reserved
-      </div>
- 
     </div>
-  </footer><!-- End Footer -->
+  </section>
+
+
+
+
+<!-- ======= Footer ======= -->
+<footer id="footer">
+  <section id="followus">
+  <div class="container" >
+    <h1>Follow us on social media for updates</h1>
+    <div class="social-links">
+      <a href="https://www.youtube.com/channel/UCjWT7ulRfJ4W0MjnGF4lyDg" class="youtube"><i class="bx bxl-youtube"></i></a>
+      <a href="https://www.facebook.com/CodeCamp.ma" class="facebook"><i class="bx bxl-facebook"></i></a>
+      <a href="https://instagram/codecamp.ma" class="instagram"><i class="bx bxl-instagram"></i></a>
+      <a href="https://linkedin.com/company/code-camp-ma" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+    </div>
+    <div class="copyright">
+      &copy; Copyright <strong><span>Code Camp</span></strong>. All Rights Reserved
+    </div>
+
+  </div>
+</section>
+
+</footer><!-- End Footer -->
 
   <div id="preloader"></div>
 
