cb73a0ecf9dc66d3a6834e40751b5e18e9e6398a
mrsbaai <abdelilah.sbaai@gmail.com>
2020-12-27 15:05:32 +0100

    ip

--- resources/views/coworking.blade.php
+++ resources/views/coworking.blade.php
@@ -158,13 +158,13 @@
       <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-            <img class="d-block w-100" src="..." alt="First slide">
+            <img class="d-block w-100" src="/construction/img/00.jpg">
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Second slide">
+            <img class="d-block w-100" src="/construction/img/01.jpg">
           </div>
           <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Third slide">
+            <img class="d-block w-100" src="/construction/img/02.jpg">
           </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
