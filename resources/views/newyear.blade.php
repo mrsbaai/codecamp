da358991c1bab4889c1709bf705ea189079289fd
mrsbaai <abdelilah.sbaai@gmail.com>
2020-12-27 21:51:01 +0100

    Update newyear.blade.php

--- resources/views/newyear.blade.php
+++ resources/views/newyear.blade.php
@@ -486,9 +486,24 @@
                 <li class="list-timeline-item p-0 pb-3 pb-lg-4 d-flex flex-wrap flex-column">
                   <p class="my-0 flex-fw text-sm text-uppercase"><span class="text-inverse op-8">09:00 - 10:00</span> - Registration</p>
                 </li>
+                <li class="list-timeline-item p-0 pb-3 pb-lg-4 d-flex flex-wrap flex-column">
+                  <p class="my-0 flex-fw text-sm text-uppercase"><span class="text-inverse op-8">09:00 - 10:00</span> - A word of welcome
+                  </p>
+                </li>
                 <li class="list-timeline-item p-0 pb-3 pb-lg-4 d-flex flex-wrap flex-column" data-toggle="collapse" data-target="#day-2-item-2">
-                  <p class="my-0 flex-fw text-sm text-uppercase"><span class="text-inverse op-8">10:00 - 12:00</span> - Vitaly Friedmann</p>
-                  <p class="my-0 collapse flex-fw text-uppercase text-xs  op-8" id="day-2-item-2"> Talk: Wireframing / <span class="text-primary">Room 19</span> </p>
+                  <p class="my-0 flex-fw text-sm text-uppercase"><span class="text-inverse op-8">10:00 - 12:00</span> - Attendants will have the chance to talk about 3 points for 5 minutes
+                  </p>
+                  <p class="my-0 flex-fw text-xs  op-8" id="day-2-item-2"> Talk: Wireframing / <span class="text-primary">Room 19</span> </p>
+                </li>
+                <li class="list-timeline-item p-0 pb-3 pb-lg-4 d-flex flex-wrap flex-column">
+                  <p class="my-0 flex-fw text-sm text-uppercase"><span class="text-inverse op-8">09:00 - 10:00</span> - Coffee break </p>
+                </li>
+                <li class="list-timeline-item p-0 pb-3 pb-lg-4 d-flex flex-wrap flex-column">
+                  <p class="my-0 flex-fw text-sm text-uppercase"><span class="text-inverse op-8">09:00 - 10:00</span> - Open Forum</p>
+                </li>
+                <li class="list-timeline-item p-0 pb-3 pb-lg-4 d-flex flex-wrap flex-column">
+                  <p class="my-0 flex-fw text-sm text-uppercase"><span class="text-inverse op-8">09:00 - 10:00</span> -  A closing word 
+                  </p>
                 </li>
                 
               </ul>
