7bedd66959374a0fd47ee482d14b8f500ff68646
mrsbaai <abdelilah.sbaai@gmail.com>
2021-01-14 12:11:00 +0100

    Update coworking.blade.php

--- resources/views/coworking.blade.php
+++ resources/views/coworking.blade.php
@@ -163,7 +163,7 @@
     <div class="col-md-2"></div>
     <div class="col-md-8">
     <iframe  width="100%" height="100%" src="https://www.youtube.com/embed/e8fkmpQRHDI?autoplay=1?&loop=1" frameborder="0" allow="accelerometer; autoplay; loop; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
-    <div class="col-md-8">
+    </div>
     <div class="col-md-2"></div>
     <br/><br/>
     <h4>Follow us on social media for updates:</h4>
