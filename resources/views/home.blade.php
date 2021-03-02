ed06d1e8574a7d079cc7381898691a4f43018ae9
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-02 19:00:25 +0100

    Revert "up"
    This reverts commit 3a974d8c8305382897013a82e8ea875bb7179a59.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,13 @@
 <section class="container">
     <div class="row vh-100">
         <div class="col-12 my-auto">
-            <h1>home</h1>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <h1>home</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
