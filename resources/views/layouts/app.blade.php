f6c1f26e40a1da14512a0cc3a72f4e63a009fc84
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-02 19:12:09 +0100

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,13 @@
         </div>
     </nav>
     <main>
-         @yield('content')
+        <section class="container">
+            <div class="row vh-100">
+                <div class="col-12 my-auto">
+                    @yield('content')
+                </div>
+            </div>
+        </section>
     </main>
         <footer id="footer" class="bg-dark text-light py-5">
         <div class="container py-5">
