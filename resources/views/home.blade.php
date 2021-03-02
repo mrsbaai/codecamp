3a974d8c8305382897013a82e8ea875bb7179a59
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-02 18:39:18 +0100

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,7 @@
 <section class="container">
     <div class="row vh-100">
         <div class="col-12 my-auto">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8">
-                        <h1>home</h1>
-                    </div>
-                </div>
-            </div>
+            <h1>home</h1>
         </div>
     </div>
 </section>
