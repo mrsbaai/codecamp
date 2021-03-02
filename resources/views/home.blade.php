2d6b761cbb665f9649a662b731fc81ee294f8b1a
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-02 19:00:12 +0100

    Revert "uo"
    This reverts commit 7343275a26f9997d52ad0ddde98c7ce820aad4be.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,25 +5,7 @@
 <section class="container">
     <div class="row vh-100">
         <div class="col-12 my-auto">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8">
-                        <div class="card">
-                            <div class="card-header">{{ __('Dashboard') }}</div>
-
-                            <div class="card-body">
-                                @if (session('status'))
-                                    <div class="alert alert-success" role="alert">
-                                        {{ session('status') }}
-                                    </div>
-                                @endif
-
-                                {{ __('You are logged in!') }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <h1>home</h1>
         </div>
     </div>
 </section>
