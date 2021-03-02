a774e8e3d9cecf53fb0f12a99b8c1e0c4c181cab
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-02 20:03:39 +0100

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="container">
-    <div class="row vh-100">
-        <div class="col-12 my-auto">
+
             <h1>Hello</h1>
-        </div>
-    </div>
-</section>
+
 @endsection
