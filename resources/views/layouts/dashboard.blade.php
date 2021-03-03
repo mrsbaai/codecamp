bdac0be61cd95f9f16d9c352808decf4737170fd
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-03 19:43:43 +0100

    fix

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -108,8 +108,8 @@
         </div>
     </div>
 
-    <footer id="footer" class="bg-dark text-light py-5">
-        blabla
+    <footer id="footer" class="bg-dark text-light py-5 text-center">
+        © Copyright CodeCamp.ma Wilaya Center ET5 N°55 TETOUAN. All Rights Reserved 
     </footer> 
     <!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
