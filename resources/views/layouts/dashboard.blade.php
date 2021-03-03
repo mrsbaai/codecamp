b99826493dba1310507e459782707362b36ef41c
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-03 18:39:33 +0100

    up

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -8,12 +8,15 @@
 
     <title>Collapsible sidebar using Bootstrap 4</title>
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-    <link href="//cdnjs.cloudflare.com/ajax/libs/ionicons/3.0.0/css/ionicons.css" rel="stylesheet" />
+
    
     <link rel="stylesheet" href="{{ asset('css/codecamp_theme.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
 
-   </head>
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+</head>
 
 <body>
     <div class="wrapper">
