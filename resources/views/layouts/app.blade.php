7eb42cac7d747e95809b5226e8c4591f48dfd3c0
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-03 16:13:32 +0100

    up

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{ asset('img/logo_sm_txt.png') }}" height="30" width="auto" alt="">
+                <img src="{{ asset('img/logo_sm_txt_1.png') }}" height="30" width="auto" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
