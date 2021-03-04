52788e91056a277909416f7f2b2eb50373d446bf
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-04 10:44:14 +0100

    Update dashboard.blade.php

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -109,7 +109,7 @@
     </div>
 
     <footer id="footer" class="bg-dark text-light py-5 text-center">
-        © Copyright CodeCamp.ma Wilaya Center ET5 N°55 TETOUAN. All Rights Reserved 
+        CodeCamp.ma Made With <span style="color: #FF304F;"><i class="fas fa-heart"></i></span>At Wilaya Center ET5 N°55 TETOUAN.
     </footer> 
     <!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
