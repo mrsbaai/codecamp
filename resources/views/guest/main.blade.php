4e3a2610d652aad4cf0f95682b9f3b4a7426e767
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-06 14:27:27 +0100

    Update main.blade.php

--- resources/views/guest/main.blade.php
+++ resources/views/guest/main.blade.php
@@ -4,17 +4,17 @@
 <div class="bg-primary">
     <div class="container">
         <div class="row align-items-center vh-100 py-5">
-            <div class="col-xl-4 col-lg-6 col mr-auto order-1 order-lg-last align-items-center justify-content-center justify-content-lg-end d-flex">
+            <div class="col-xl-4 col-lg-6 col mr-auto order-1 order-lg-2 align-items-center justify-content-center justify-content-lg-end d-flex">
                 <div class="img-fluid text-white">
                    <img src="{{ asset('img/logo_transparent.png') }}" width="240" height="auto" alt="Codecamp.ma">
                 </div>
-           </div>
+            </div>
 
-            <div class="col-xl-5 col-lg-6 ml-auto py-2 text-light order-2 text-center text-lg-left">
+            <div class="col-xl-5 col-lg-6 ml-auto py-2 text-light order-last text-center text-lg-left">
                 <h1 class="mb-0">
                     <span>A Moroccan Community Initiative</span>
                 </h1>
-                <p class="lead mb-3"><span>T highlight modern carer paths & informational technologies.</span></p>
+                <p class="lead mb-3"><span>To highlight modern carer paths & informational technologies.</span></p>
                 <a class="btn btn-outline-light btn-lg mr-2" href="http://themestr.app" target="_new">Free Account</a> 
                                                 <div class="btn-group">
                               <a href="theme.css" class="btn btn-outline-light btn-lg">
