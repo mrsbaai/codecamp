cc2f7c7184543fc126c276fe15d11886685eccc1
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-06 14:24:06 +0100

    Update main.blade.php

--- resources/views/guest/main.blade.php
+++ resources/views/guest/main.blade.php
@@ -4,6 +4,12 @@
 <div class="bg-primary">
     <div class="container">
         <div class="row align-items-center vh-100 py-5">
+            <div class="col-xl-4 col-lg-6 col mr-auto order-1 order-lg-last align-items-center justify-content-center justify-content-lg-end d-flex">
+                <div class="img-fluid text-white">
+                   <img src="{{ asset('img/logo_transparent.png') }}" width="240" height="auto" alt="Codecamp.ma">
+                </div>
+           </div>
+           
             <div class="col-xl-5 col-lg-6 ml-auto py-2 text-light order-2 text-center text-lg-left">
                 <h1 class="mb-0">
                     <span>A Moroccan Community Initiative</span>
@@ -26,11 +32,7 @@
                               </div>
                             </div>  
             </div>
-            <div class="col-xl-4 col-lg-6 col mr-auto order-1 order-lg-last align-items-center justify-content-center justify-content-lg-end d-flex">
-                 <div class="img-fluid text-white">
-                    <img src="{{ asset('img/logo_transparent.png') }}" width="240" height="auto" alt="Codecamp.ma">
-                 </div>
-            </div>
+
         </div>
     </div>
 </div>
