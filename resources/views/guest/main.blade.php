e4009f911c6ee1c7ff681e353bb3debfd31bda37
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-06 14:38:17 +0100

    up

--- resources/views/guest/main.blade.php
+++ resources/views/guest/main.blade.php
@@ -1,41 +1,49 @@
 @extends('layouts.app')
 
 @section('pre')
-<div class="bg-primary">
-    <div class="container">
-        <div class="row align-items-center vh-100 py-5">
-            <div class="col-xl-4 col-lg-6 col mr-auto order-1 order-lg-2 align-items-center justify-content-center justify-content-lg-start d-flex">
-                <div class="img-fluid text-white">
-                   <img src="{{ asset('img/logo_transparent.png') }}" width="240" height="auto" alt="Codecamp.ma">
+<header class="bg-primary">
+    <div class="container h-100">
+        <div class="row h-100">
+            <div class="col-12">
+                <div class="text-center m-0 vh-100 d-flex flex-column justify-content-center text-light">
+                    <div class="img-fluid text-white">
+                        <img src="{{ asset('img/logo_transparent.png') }}" width="240" height="auto" alt="Codecamp.ma">
+                     </div>
+
+                    <h1 class="display-4">A Moroccan Community Initiative</h1>
+                    <p class="lead">To highlight modern carer paths & informational technologies.</p>
+                    <div class="row">
+                        <div class="col-lg-4 col-sm-6 mx-auto">
+                            <div class="input-group mb-3">
+                              <input type="text" class="form-control form-control-lg" placeholder="your@email">
+                              <div class="input-group-append">
+                                <button class="btn btn-outline-light btn-lg rounded-right" type="button">Sign-up</button>
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-2 mx-auto">
+                            <div class="btn-group">
+                                <a href="#" class="btn btn-outline-light btn-lg">
+                                    Services
+                                  </a>
+                                <button type="button" class="btn btn-lg btn-outline-light dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <span class="sr-only">Services</span>
+                                  </button>
+                                  <div class="dropdown-menu">
+                                      <a class="dropdown-item" target="new" href="{{ route('ShowJob') }}">{{ __('Find a job') }}</a>
+                                      <div class="dropdown-divider"></div>
+                                      <a class="dropdown-item" target="new" href="{{ route('ShowEvents') }}">{{ __('Live Events') }}</a>
+                                      <div class="dropdown-divider"></div>
+                                      <a class="dropdown-item" target="new" href="{{ route('ShowCoworking') }}">{{ __('Coworking') }}</a>
+                                  </div>
+                            </div> 
+                    </div>
                 </div>
             </div>
-
-            <div class="col-xl-5 col-lg-6 ml-auto py-2 text-light order-last text-center text-lg-left">
-                <h1 class="mb-0">
-                    <span>A Moroccan Community Initiative</span>
-                </h1>
-                <p class="lead mb-3"><span>To highlight modern carer paths & informational technologies.</span></p>
-                <a class="btn btn-outline-light btn-lg mr-2" href="http://themestr.app" target="_new">Free Account</a> 
-                                                <div class="btn-group">
-                              <a href="theme.css" class="btn btn-outline-light btn-lg">
-                                Services
-                              </a>
-                              <button type="button" class="btn btn-lg btn-outline-light dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="sr-only">Services</span>
-                              </button>
-                              <div class="dropdown-menu">
-                                  <a class="dropdown-item" target="new" href="{{ route('ShowJob') }}">{{ __('Find a job') }}</a>
-                                  <div class="dropdown-divider"></div>
-                                  <a class="dropdown-item" target="new" href="{{ route('ShowEvents') }}">{{ __('Live Events') }}</a>
-                                  <div class="dropdown-divider"></div>
-                                  <a class="dropdown-item" target="new" href="{{ route('ShowCoworking') }}">{{ __('Coworking') }}</a>
-                              </div>
-                            </div>  
-            </div>
-
         </div>
     </div>
-</div>
+</header>
 @endsection
 
 
