e730a74530a7ab3ece243eda209a4b9b2cc582a0
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-08 16:32:48 +0100

    Update home.blade.php

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -48,7 +48,7 @@
                             <h1 class="display-2 text-primary"><i class="fas fa-laptop-code"></i></h1>
                             <h4 class="card-title text-primary">Freelance</h4>
                             <p class="card-text">Get exposure to job opportunities, and find freelance projects to be part of.</p>
-                            <a href="#" class="btn btn-primary mt-auto copyable">Make a potfolio?</a>
+                            <a href="#" class="btn btn-primary mt-auto copyable">Make a potfolio</a>
                         </div>
                     </div>
                 </div>
@@ -58,7 +58,7 @@
                             <h1 class="display-2 text-primary"><i class="fas fa-hat-wizard"></i></h1>
                             <h4 class="card-title text-primary">Learn</h4>
                             <p class="card-text">Get access to free online learning material and get notified about new online and offline events.</p>
-                            <a href="#" class="btn btn-primary mt-auto copyable">Next Event?</a>
+                            <a href="#" class="btn btn-primary mt-auto copyable">Next Event</a>
                         </div>
                     </div>
                 </div>
@@ -75,7 +75,49 @@
             </div>
         </div>
     </div>
-<div></div></section>
+
+
+    <div class="col-12 my-auto text-center py-5">
+        <h1>Latest Episodes</h1>
+    </div>
+
+
+    <div class="row">
+        <div class="col-lg-4 mb-4">
+            <div class="card border-primary h-100 copyable">
+                <div class="card-body d-flex flex-column align-items-start">
+                    <h4 class="card-title text-primary">Card Outline</h4>
+                    <p class="card-text">Themestr.app makes it easy to customize this, and other free Bootstrap 4 themes for your downloading pleasure.</p>
+                    <a href="#" class="btn btn-primary mt-auto copyable">Button<a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></a>
+                </div>
+            <a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></div>
+        </div>
+        <div class="col-lg-4 mb-4">
+            <div class="card border-primary h-100 copyable">
+                <div class="card-body">
+                    <div class="list-group copyable">
+                        <a href="#" class="list-group-item list-group-item-action active">Dis List Group</a>
+                        <a href="#" class="list-group-item list-group-item-action">Custom Colors</a>
+                        <a href="#" class="list-group-item list-group-item-action">Google Fonts</a>
+                        <a href="#" class="list-group-item list-group-item-action disabled">Ionicons</a>
+                    <a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></div>
+                </div>
+            <a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></div>
+        </div>
+        <div class="col-lg-4 mb-4">
+            <div class="card bg-primary text-white h-100 copyable">
+                <div class="card-body d-flex flex-column align-items-start">
+                    <h4 class="card-title">Scrollspy</h4>
+                    <p class="card-text">The top NavBar utilizes Bootstrap's Scrollspy component. You'll notice when you scroll the page the "active" nav section is highlighted.</p>
+                    <a href="https://getbootstrap.com/docs/4.1/components/scrollspy/" class="btn btn-primary border-white mt-auto copyable">Read More<a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></a>
+                </div>
+            <a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></div>
+        </div>
+    </div>
+
+
+
+</section>
 
 @endsection
 
