b4f89472afffe613bf010be5b50f48f236b3bd45
mrsbaai <abdelilah.sbaai@gmail.com>
2021-03-08 15:23:08 +0100

    Update home.blade.php

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -33,46 +33,50 @@
 
 
 @section('content')
-<div class="row vh-100">
 
-    <div class="col-12 my-auto">
-        <h1>Why Get An Account?</h1>
-    </div>
-    <div class="col-12 my-auto">
-        <div class="row text-center">
-            <div class="col-lg-4 mb-4">
-                <div class="card h-100 copyable">
-                    <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                        <h1 class="display-2 text-primary"><span class="ion ion-ios-snow-outline"></span></h1>
-                        <h4 class="card-title text-primary">Find Work</h4>
-                        <p class="card-text">Don't like that typical Bootstrap look? Each theme has a custom look-and-feel, while maximizing use of Bootstrap without extensive additional CSS &amp; JS.</p>
-                        <a href="#" class="btn btn-primary mt-auto copyable">More<a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></a>
-                    </div>
-                <a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></div>
-            </div>
-            <div class="col-lg-4 mb-4">
-                <div class="card h-100 copyable">
-                    <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                        <h1 class="display-2 text-primary"><span class="ion ion-ios-tablet-portrait-outline"></span></h1>
-                        <h4 class="card-title text-primary">Learn</h4>
-                        <p class="card-text">Based on mobile-first Bootstrap 4, all themes are 100% responsive and designed to look great on everything from smartphones to tablets to desktops..</p>
-                        <a href="#" class="btn btn-primary mt-auto copyable">More<a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></a>
-                    </div>
-                <a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></div>
-            </div>
-            <div class="col-lg-4 mb-4">
-                <div class="card h-100 copyable">
-                    <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                        <h1 class="display-2 text-primary"><span class="ion ion-ios-checkmark-circle-outline"></span></h1>
-                        <h4 class="card-title text-primary">Coworking</h4>
-                        <p class="card-text">Themes are crafted with care for design and performance. All themes are built on modern HTML5 &amp; CSS3 standards to ensure consistency and cross-browser support.</p>
-                        <a href="#" class="btn btn-primary mt-auto copyable">More<a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></a>
-                    </div>
-                <a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></div>
+<section class="container">
+
+    <div class="row vh-100">
+        <div class="col-12 my-auto text-center">
+            <h1>Why Get An Account?</h1>
+        </div>
+        <div class="col-12 my-auto">
+            <div class="row text-center">
+                <div class="col-lg-4 mb-4">
+                    <div class="card h-100 copyable">
+                        <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                            <h1 class="display-2 text-primary"><i class="fas fa-project-diagram"></i></h1>
+                            <h4 class="card-title text-primary">Freelance</h4>
+                            <p class="card-text">Don't like that typical Bootstrap look? Each theme has a custom look-and-feel, while maximizing use of Bootstrap without extensive additional CSS &amp; JS.</p>
+                            <a href="#" class="btn btn-primary mt-auto copyable">Button<a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></a>
+                        </div>
+                    <a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></div>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <div class="card h-100 copyable">
+                        <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                            <h1 class="display-2 text-primary"><span class="ion ion-ios-tablet-portrait-outline"></span></h1>
+                            <h4 class="card-title text-primary">Learn</h4>
+                            <p class="card-text">Based on mobile-first Bootstrap 4, all themes are 100% responsive and designed to look great on everything from smartphones to tablets to desktops..</p>
+                            <a href="#" class="btn btn-primary mt-auto copyable">Button<a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></a>
+                        </div>
+                    <a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></div>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <div class="card h-100 copyable">
+                        <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                            <h1 class="display-2 text-primary"><span class="ion ion-ios-checkmark-circle-outline"></span></h1>
+                            <h4 class="card-title text-primary">Coworking</h4>
+                            <p class="card-text">Themes are crafted with care for design and performance. All themes are built on modern HTML5 &amp; CSS3 standards to ensure consistency and cross-browser support.</p>
+                            <a href="#" class="btn btn-primary mt-auto copyable">Button<a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></a>
+                        </div>
+                    <a href="" class="copyable-trigger" data-original-title="" title="">&lt;&gt;</a></div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+<div></div></section>
+
 @endsection
 
 
