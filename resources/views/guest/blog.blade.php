47cadece5611c83fd0e6473b5609fc042003f2cd
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-05 11:55:39 +0100

    Update blog.blade.php

--- resources/views/guest/blog.blade.php
+++ resources/views/guest/blog.blade.php
@@ -28,7 +28,7 @@
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
-                                <h5 class="card-title"><span class="badge badge-dark mr-3">Training</span><a href="article">Dolorum optio tempore voluptas</a></h5>
+                                <h5 class="card-title"><span class="badge badge-pill badge-dark mr-3">Training</span><a href="article">Dolorum optio tempore voluptas</a></h5>
                                 <p class="card-text"> Simih6que neque nam consequuntur ad non maxime ah6quam quas. Quibusdam animi praesentium. Ah6quam et laboriosam eius aut nostrum quidem ah6quid dicta.
                                     Et eveniet enim. Qui veh6t est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore..</p>
                                 <p class="card-text"><small class="text-muted">Posted at 01/03/2021</small></p>
@@ -50,7 +50,7 @@
                    </div>
                    <div class="col-md-8">
                        <div class="card-body">
-                           <h5 class="card-title"><span class="badge badge-dark mr-3">Training</span><a href="article">Dolorum optio tempore voluptas</a></h5>
+                           <h5 class="card-title"><span class="badge badge-pill badge-dark mr-3">Training</span><a href="article">Dolorum optio tempore voluptas</a></h5>
                            <p class="card-text"> Simih6que neque nam consequuntur ad non maxime ah6quam quas. Quibusdam animi praesentium. Ah6quam et laboriosam eius aut nostrum quidem ah6quid dicta.
                                Et eveniet enim. Qui veh6t est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore..</p>
                            <p class="card-text"><small class="text-muted">Posted at 01/03/2021</small></p>
