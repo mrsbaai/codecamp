cc6af905e554cafd9153c618d71164672be0687a
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-05 11:42:01 +0100

    Update blog.blade.php

--- resources/views/guest/blog.blade.php
+++ resources/views/guest/blog.blade.php
@@ -24,7 +24,7 @@
                  <div class="card mb3-3">
                     <div class="row no-gutters">
                         <div class="col-md-4">
-                          <img src="/img/square.jpg" class="card-img-left" alt="Dolorum optio tempore voluptas">
+                          <img src="/img/square.jpg" class="card-img card-img-left" alt="Dolorum optio tempore voluptas">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
@@ -41,27 +41,7 @@
                     </div>
                </div>
           </article>
-          <article>
-            <div class="card mb3-3">
-               <div class="row no-gutters">
-                   <div class="col-md-4">
-                     <img src="/img/square.jpg" class="card-img-left" alt="Dolorum optio tempore voluptas">
-                   </div>
-                   <div class="col-md-8">
-                       <div class="card-body">
-                           <h5 class="card-title"><span class="badge badge-info  mr-3">badge</span><a href="article">Dolorum optio tempore voluptas</a></h5>
-                           <p class="card-text"> Simih6que neque nam consequuntur ad non maxime ah6quam quas. Quibusdam animi praesentium. Ah6quam et laboriosam eius aut nostrum quidem ah6quid dicta.
-                               Et eveniet enim. Qui veh6t est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore..</p>
-                           <p class="card-text"><small class="text-muted">Posted at 01/03/2021</small></p>
-
-                               <div class="col-12 text-right p-0">
-                                   <a href="/blog" class="btn btn-dark btn-sm mt-auto">Read More</a>
-                               </div> 
-                       </div>
-                   </div>
-               </div>
-          </div>
-     </article>
+          
 
 
              
