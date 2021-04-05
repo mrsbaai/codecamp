28743b0fc46063170e8529c4f42a1973a98ab3d2
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-05 11:19:19 +0100

    Update blog.blade.php

--- resources/views/guest/blog.blade.php
+++ resources/views/guest/blog.blade.php
@@ -24,27 +24,33 @@
                  <div class="card mb3-3">
                    <img src="{{ asset('img/blogg.jpg') }}" class="card-img-top" alt="...">
                    <div class="card-body">
-                      <h5 class="card-title"><a href="article">Dolorum optio tempore voluptas</a></h5>
+                      <h5 class="card-title"><span class="badge badge-ingo">badge</span><a href="article">Dolorum optio tempore voluptas</a></h5>
                       <p class="card-text"> Simih6que neque nam consequuntur ad non maxime ah6quam quas. Quibusdam animi praesentium. Ah6quam et laboriosam eius aut nostrum quidem ah6quid dicta.
                            Et eveniet enim. Qui veh6t est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore..</p>
-                           <p class="card-text">Event- <small class="text-muted">01/03/2021</small></p>
-                      <a href="eventdetails" class="btn btn-primary">Read more</a>
+                          <span class="text-left"> <p class="card-text"><small class="text-muted">01/03/2021</small></p></span>
+                          <span class="text-right"> <a href="eventdetails" class="btn btn-primary">Read more</a></span>
+                          
+                     
                    </div>
                </div>
           </article>
            <br/>
-             <article>
-               <div class="card mb-3">
-                  <img src="{{ asset('img/blogg.jpg') }}" class="card-img-top" alt="...">
-                   <div class="card-body">
-                      <h5 class="card-title"><a href="article">Dolorum optio tempore voluptas</a></h5>
-                      <p class="card-text"> Simih6que neque nam consequuntur ad non maxime ah6quam quas. Quibusdam animi praesentium. Ah6quam et laboriosam eius aut nostrum quidem ah6quid dicta.
-                           Et eveniet enim. Qui veh6t est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore..</p>
-                           <p class="card-text">Training- <small class="text-muted">01/03/2021</small></p>
-                      <a href="article" class="btn btn-primary">Read more</a>
-                   </div>
-               </div>
-             </article>
+           <article>
+            <div class="card mb3-3">
+              <img src="{{ asset('img/blogg.jpg') }}" class="card-img-top" alt="...">
+              <div class="card-body">
+                 <h5 class="card-title"><span class="badge badge-ingo">badge</span><a href="article">Dolorum optio tempore voluptas</a></h5>
+                 <p class="card-text"> Simih6que neque nam consequuntur ad non maxime ah6quam quas. Quibusdam animi praesentium. Ah6quam et laboriosam eius aut nostrum quidem ah6quid dicta.
+                      Et eveniet enim. Qui veh6t est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore..</p>
+                     <span class="text-left"> <p class="card-text"><small class="text-muted">01/03/2021</small></p></span>
+                     <span class="text-right"> <a href="eventdetails" class="btn btn-primary">Read more</a></span>
+                     
+                
+              </div>
+          </div>
+     </article>
+
+             
          </div>
          
       </div>
