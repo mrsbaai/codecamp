45abc5483ddc9f601e7d28fff788b8079ea8c0c4
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-05 12:14:59 +0100

    Update contact.blade.php

--- resources/views/guest/contact.blade.php
+++ resources/views/guest/contact.blade.php
@@ -57,13 +57,8 @@
           </div>
 
         </div>
-        
-        <div class="row mt-1">
-        <div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
-            <iframe src="https://maps.google.com/maps?q=manhatan&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
-              style="border:0" allowfullscreen></iframe>
-          </div>
-        </div>
+
+    
 
       </div>
 
