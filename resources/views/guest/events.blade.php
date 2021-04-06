90a4d8d4196e38ad49c9ede7929e22781d074bf2
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-06 18:14:38 +0100

    Update events.blade.php

--- resources/views/guest/events.blade.php
+++ resources/views/guest/events.blade.php
@@ -60,46 +60,59 @@
 
         <!--Body-->
         <div class="modal-body">
-           <div class="card-body">
-                <h5 class="card-title"><span class="badge badge-pill badge-dark mr-3">190 Dh</span> Dolorum optio tempore </h5>
-               <h6 class="card-subtitle mb-2 text-muted">
-                   <div class="row">
-                     <div class="col-6 col-md-3 mt-2 ">
-                         <p class="card-text"><i class="fas fa-calendar-alt "></i><small class="text-muted"> 01/03/2021</small></p>
+            <div class="card-body">
+                <h5 class="card-title mt-2">2021 New Year Online Business Resolution</h5>
+                <h6 class="card-subtitle mb-2">
+                  <div class="row">
+                      <div class="col-12 mt-1">
+                          <p class="card-text"><i class="fas fa-map-marker-alt"></i><small> Wilaya Center ET5 N°55 TETOUAN MOROCCO</small></p>
+                      </div>
+                      <div class="col-12 mt-1">
+                          <p class="card-text"><i class="fas fa-calendar-alt "></i><small> 01/03/2021 - 01/03/2021</small></p>
                      </div>
-                      <div class="col-6 col-md-3  mt-2">
-                         <p class="card-text"><i class="fas fa-clock"></i> <small class="text-muted"> 03:00-5:00</small></p>
-                       </div>
-                     <div class="col-6 col-md-6  mt-2">
-                          <p class="card-text"><i class="fas fa-map-marker-alt"></i> <small class="text-muted"> Wilaya Center ET5 N°55 TETOUAN MOROCCO</small></p>
+      
+                    <div class="col-12 mt-1">
+                         <p class="card-text"><i class="fas fa-clock "></i><small> 15:00 - 18:00</small></p>
+                    </div>
+       
+                    <div class="col-12 mt-1">
+                     <p class="card-text"><i class="fas fa-money-bill-wave"></i><small> 190 DH</small></p>
                      </div>
-                   </div>
+      
+      
+                    
+                    </div>
+                </h6>
 
-               </h6>
-               <p class="card-text mt-2">Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-             </div>
-             <div class="form-row">
-                <div class="col-lg-4 form-group">
-                     <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                     <div class="validate"></div>
+                <div class="form-row">
+                    <div class="col-lg-4 form-group">
+                         <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                         <div class="validate"></div>
+                     </div>
+                     <div class="col-lg-4 form-group">
+                          <input type="text" name="phone" class="form-control" id="phone" placeholder="Your Phone Number" data-rule="minlen:8" data-msg="Please enter your realphone number" />
+                          <div class="validate"></div>
+                     </div>
+                     <div class="col-lg-4 form-group">
+                          <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                          <div class="validate"></div>
+                     </div>
+                     
                  </div>
-                 <div class="col-lg-4 form-group">
-                      <input type="text" name="phone" class="form-control" id="phone" placeholder="Your Phone Number" data-rule="minlen:8" data-msg="Please enter your realphone number" />
-                      <div class="validate"></div>
-                 </div>
-                 <div class="col-lg-4 form-group">
-                      <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                      <div class="validate"></div>
-                 </div>
-                 
-             </div>
+
+              </div>
+              
+
+
         </div>
 
-        <!--Footer-->
-         <div class="modal-footer">
-           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-           <button type="button" class="btn btn-primary">Submit</button>
-         </div>
+                <!--Footer-->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-dark">Submit</button>
+                  </div>
+
+
 
       </div>
       <!--/.Content-->
