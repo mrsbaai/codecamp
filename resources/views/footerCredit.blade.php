e2c07fa17a0184267f5de3b2f0de94576190f19e
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-07 13:49:40 +0100

    up

--- resources/views/footerCredit.blade.php
+++ resources/views/footerCredit.blade.php
@@ -1,21 +1,6 @@
 @section('footerCredit')
-<div class="text-center social-links">
-    <a href="https://www.facebook.com/CodeCamp.ma">
-        <span  class="fab fa-facebook-f"></span>
-    </a>
-    <a href="https://linkedin.com/company/code-camp-ma">
-        <span  class="fab fa-linkedin"></span>
-    </a>
-    <a href="https://www.youtube.com/channel/UCjWT7ulRfJ4W0MjnGF4lyDg">
-        <span  class="fab fa-youtube"></span>
-    </a>
-    <a href="https://instagram/codecamp.ma" >
-        <span  class="fab fa-instagram"></span>
-    </a>
 
-
-</div>
-<div class="text-center social-links">
+<div class="p-4">
     Made With&nbsp;<span style="color: #FF304F;"><i class="fas fa-heart"></i></span>&nbsp;At Wilaya Center ET5 N°55 TETOUAN MOROCCO
 </div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -128,7 +128,7 @@
           <!--Grid row-->
           <div class="row">
             <!--Grid column-->
-            <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
                 <h4 class="mb-4">Receive news about upcoming events</h4>
               <div class="input-group mb-3 pr-4 pl-4">
                 <input type="text" class="form-control form-control-lg " placeholder="your@email">
@@ -141,27 +141,9 @@
             </div>
             <!--Grid column-->
     
-            <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
-                <h4>Follow Us</h4>
-    
-                <ul class="list-unstyled mb-0">
-                  <li>
-                     <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i>Facebook</a>
-                  </li>
-                  <li>
-                     <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i>Facebook</a>
-                  </li>
-                  <li>
-                     <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i>Facebook</a>
-                  </li>
-                  <li>
-                     <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i class="fab fa-facebook-f"></i>Facebook</a>
-                  </li>
-                </ul>
-            </div>
 
             <!--Grid column-->
-            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
               <h4>Services</h4>
     
               <ul class="list-unstyled mb-0">
@@ -182,7 +164,7 @@
             <!--Grid column-->
     
             <!--Grid column-->
-            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
               <h4>Links</h4>
     
               <ul class="list-unstyled">
@@ -210,6 +192,42 @@
           <!--Grid row-->
         </div>
         <!-- Grid container -->
+
+        <div class="container p-4 pb-0">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+              <!-- Facebook -->
+              <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i class="fab fa-facebook-f"></i
+              ></a>
+        
+              <!-- Twitter -->
+              <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i class="fab fa-twitter"></i
+              ></a>
+        
+              <!-- Google -->
+              <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i class="fab fa-google"></i
+              ></a>
+        
+              <!-- Instagram -->
+              <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i class="fab fa-instagram"></i
+              ></a>
+        
+              <!-- Linkedin -->
+              <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i class="fab fa-linkedin-in"></i
+              ></a>
+        
+              <!-- Github -->
+              <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
+                ><i class="fab fa-github"></i
+              ></a>
+            </section>
+            <!-- Section: Social media -->
+          </div>
     
         <!-- Copyright -->
         <div class="p-3" style="background-color: black">
