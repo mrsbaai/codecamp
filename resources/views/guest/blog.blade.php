0cdfe932b21bb51113e3780af7cfc8f60d0b5c89
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-07 12:26:58 +0100

    Update blog.blade.php

--- resources/views/guest/blog.blade.php
+++ resources/views/guest/blog.blade.php
@@ -31,22 +31,27 @@
     </div>
 
 
+    <div class="card-header">
+        2021 New Year Online Business Resolution
+    </div>
     <div class="card-body ml-3">
-      <h5 class="card-title mt-2">2021 New Year Online Business Resolution</h5>
+      <h5 class="card-title mt-2"></h5>
 
       <p class="card-text mt-2 mb-2">
         We are organizing a professional gathering to discuss and exchange ideas about how can we make our online businesses more profitable in 2021. <a class="text-info" href="/blog/3334">More info...</a>
     </p>
 
-    <div class="col-6 text-left p-0">
-        <small class="text-muted">Posted at 01/03/2021</small>
-    </div>
+
 
     <div class="col-6 text-right p-0">
         <a href="/blog" class="btn btn-dark btn-sm mt-auto">Read More</a>
     </div>
 
     </div>
+
+    <div class="card-footer text-muted">
+        2 days ago
+      </div>
   </div>
     
 </div>
