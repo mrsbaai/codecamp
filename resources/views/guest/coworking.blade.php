12096149ff972bdeaab5c1d0ed0f24e92d7b558c
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-08 12:52:01 +0100

    Update coworking.blade.php

--- resources/views/guest/coworking.blade.php
+++ resources/views/guest/coworking.blade.php
@@ -73,6 +73,56 @@
 
 
 
+
+<div class="modal fade" id="tetoaun" tabindex="-1" role="dialog" aria-labelledby="episode title here" style="display: none;" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+
+      <!--Content-->
+      <div class="modal-content">
+
+        <!--Body-->
+        <div class="modal-body mb-0 p-0">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+
+          images here
+        </div>
+
+        <!--Footer-->
+        <div class="modal-footer justify-content-center text-center">
+            <div class="mt-3 mb-3">
+                <div class="form-row">
+                    <div class="col-lg-3 form-group">
+                         <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                         <div class="validate"></div>
+                     </div>
+                     <div class="col-lg-3 form-group">
+                          <input type="text" name="phone" class="form-control" id="phone" placeholder="Your Phone Number" data-rule="minlen:8" data-msg="Please enter your realphone number" />
+                          <div class="validate"></div>
+                     </div>
+                     <div class="col-lg-3 form-group">
+                          <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                          <div class="validate"></div>
+                     </div>
+                     <div class="col-lg-3 form-group">
+                        <button type="button" class="btn btn-primary btn-block">Request A Key</button>
+                     </div>
+
+                     
+                 </div>
+
+            </div>
+        </div>
+
+      </div>
+      <!--/.Content-->
+
+    </div>
+</div>
+
+
+
 @endsection
 
 
