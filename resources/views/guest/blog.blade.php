78843916d67b3c59ff2bd7d37d629e59eeb207d9
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-08 14:09:09 +0100

    up

--- resources/views/guest/blog.blade.php
+++ resources/views/guest/blog.blade.php
@@ -44,7 +44,61 @@
 
 
     </div>
+</div>
+
+
+<div class="card">
+  <div class="thumb-container">
+      <img class="card-img" src="img/square.jpg" alt="2021 New Year Online Business Resolution" class="thumb-image">
+      <div class="thumb-overlay">
+      <a href="/blog/3334" class="thumb-icon" title="2021 New Year Online Business Resolution">
+        <i class="fa fa-info-circle"></i>
+      </a>
+      </div>
   </div>
+
+
+  <div class="card-body ml-3">
+    <h5 class="card-title mt-2">2021 New Year Online Business Resolution</h5>
+
+    <p class="card-text mt-2 mb-2">
+      We are organizing a professional gathering to discuss and exchange ideas about how can we make our online businesses more profitable in 2021. <a class="text-info" href="/blog/3334">Read more...</a>
+  </p>
+
+  <div class="text-right p-0">
+      <small class="text-muted">Posted at 01/03/2021</small>
+  </div>
+
+
+  </div>
+</div>
+
+
+<div class="card">
+  <div class="thumb-container">
+      <img class="card-img" src="img/square.jpg" alt="2021 New Year Online Business Resolution" class="thumb-image">
+      <div class="thumb-overlay">
+      <a href="/blog/3334" class="thumb-icon" title="2021 New Year Online Business Resolution">
+        <i class="fa fa-info-circle"></i>
+      </a>
+      </div>
+  </div>
+
+
+  <div class="card-body ml-3">
+    <h5 class="card-title mt-2">2021 New Year Online Business Resolution</h5>
+
+    <p class="card-text mt-2 mb-2">
+      We are organizing a professional gathering to discuss and exchange ideas about how can we make our online businesses more profitable in 2021. <a class="text-info" href="/blog/3334">Read more...</a>
+  </p>
+
+  <div class="text-right p-0">
+      <small class="text-muted">Posted at 01/03/2021</small>
+  </div>
+
+
+  </div>
+</div>
     
 </div>
 @endsection
