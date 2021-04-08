28ca51bb4f1277eae5c951e338737b413a15cbf5
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-08 12:58:48 +0100

    Update coworking.blade.php

--- resources/views/guest/coworking.blade.php
+++ resources/views/guest/coworking.blade.php
@@ -86,7 +86,50 @@
                 <span aria-hidden="true">&times;</span>
               </button>
 
-          images here
+              <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+        
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="/construction/img/00.png">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="/construction/img/01.jpg">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="/construction/img/02.jpg">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="/construction/img/03.jpg">
+                  </div>
+         
+                  
+        
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="/construction/img/07.jpg">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="/construction/img/08.jpg">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="/construction/img/09.jpg">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="/construction/img/10.jpg">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="/construction/img/11.jpg">
+                  </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+              
         </div>
 
         <!--Footer-->
