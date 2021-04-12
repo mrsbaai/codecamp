3b03647063c272c264f553c535144cf29a5b58f7
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-12 15:30:15 +0000

    Update cam.blade.php

--- resources/views/dashboard/cam.blade.php
+++ resources/views/dashboard/cam.blade.php
@@ -1,26 +1,29 @@
 @extends('layouts.dashboard')
 
 @section('content')
+
+<div class="contaner p-0 m-0 bg-dark">
 <div class="text-center m-5">
-    <h3>TETOUAN <span class="text-green">- (live)</span></h3>
+    <h3 class="text-light">TETOUAN <span class="text-green">- (live)</span></h3>
 </div>
 
 <div class="row justify-content-center">
-    <div class="col-lg-5 mb-4">
+    <div class="col-lg-5 mb-4 text-center">
         <iframe width="290" height="240" src="https://rtsp.me/embed/RRiH8zAF/" frameborder="0"></iframe> 
     </div>
-    <div class="col-lg-5 mb-4">
+    <div class="col-lg-5 mb-4 text-center">
         <iframe width="290" height="240" src="https://rtsp.me/embed/y3dHDeke/" frameborder="0"></iframe>
     </div>
 </div>
 
 <div class="row justify-content-center">
-    <div class="col-lg-5 mb-4">
+    <div class="col-lg-5 mb-4 text-center">
         <iframe width="290" height="240" src="https://rtsp.me/embed/ibDNddKz/" frameborder="0"></iframe>      
     </div>
-    <div class="col-lg-5 mb-4">
+    <div class="col-lg-5 mb-4 text-center">
         <iframe width="290" height="240" src="https://rtsp.me/embed/EnQ5ehkK/" frameborder="0"></iframe>
     </div>
+</div>
 </div>
 
 @endsection
