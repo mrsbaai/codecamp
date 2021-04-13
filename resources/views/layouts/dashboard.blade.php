c625ed888714dcaec08c31da1457e6dad8d5c1e2
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-13 13:40:33 +0000

    Update dashboard.blade.php

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -165,15 +165,17 @@
 
             @yield('content')
 
+            <footer id="footer" class="bg-dark text-light text-center">
+                @yield('footer')
+                @yield('footerCredit')
+            </footer>
+
 
            
         </div>
 
-        <footer id="footer" class="bg-dark text-light text-center">
-            @yield('footer')
-            @yield('footerCredit')
-        </footer>
-        
+
+
     </div>
 
  
