c0dc4ed2157cc66e9fa1f2daa9cb3d380931d522
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-13 12:36:49 +0000

    Update dashboard.blade.php

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -161,16 +161,14 @@
             </nav>
 
             @yield('content')
-
            
         </div>
-        <footer id="footer" class="bg-dark text-light text-center">
-            @yield('footer')
-            @yield('footerCredit')
-        </footer> 
     </div>
 
-
+    <footer id="footer" class="bg-dark text-light text-center">
+        @yield('footer')
+        @yield('footerCredit')
+    </footer> 
     <!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <!-- Popper.JS -->
