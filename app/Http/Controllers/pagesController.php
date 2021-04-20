4e0891c375730a3dcd13b76df43d040fe4155e8b
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-20 14:39:41 +0000

    Update pagesController.php

--- app/Http/Controllers/pagesController.php
+++ app/Http/Controllers/pagesController.php
@@ -100,7 +100,7 @@ class PagesController extends Controller
 
     public function ShowCoworking(){
         //return view('guest.coworking');
-        return view('coworking');
+        return view('guest.coworking');
     }
 
 
