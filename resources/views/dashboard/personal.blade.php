32ce7210d311c91b4adc3a65eba9106da1d79aa3
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-22 16:57:49 +0000

    up

--- resources/views/dashboard/personal.blade.php
+++ resources/views/dashboard/personal.blade.php
@@ -1,10 +1,127 @@
+
 @extends('layouts.dashboard')
 
 @section('content')
-            <h1>Hello</h1>
+<div class="container">
+  <div class="row justify-content-center">
+     <div class="col-lg-8 m-3">
+         <div class="card">
+             <div class="card-header">
+                 <ul class="nav nav-tabs" id="myTab" role="tablist">
+                     <li class="nav-item">
+                         <a class="nav-link active" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="true">Profile</a>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link" id="update-tab" data-toggle="tab" href="#update" role="tab" aria-controls="update" aria-selected="false">Update</a>
+                     </li>
+                 </ul>
+              </div>
+              <div class="card-body">
+                   <div class="tab-content" id="myTabContent">
+                       <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                           <div class="row">
+                             <div class="col-sm-3">
+                                 <h6 class="mb-0"><strong>User Name</strong></h6>
+                             </div>
+                             <div class="col-sm-9 ">
+                                 {{ Auth::user()->username }}
+                             </div>
+                          </div>
+                          <hr>
+                          <div class="row">
+                             <div class="col-sm-3">
+                                 <h6 class="mb-0"><strong>First Name</strong></h6>
+                             </div>
+                             <div class="col-sm-9 ">
+                                 {{ Auth::user()->first_name }}
+                             </div>
+                          </div>
+                          <hr>
+                          <div class="row">
+                               <div class="col-sm-3">
+                                  <h6 class="mb-0"><strong>Last Name</strong></h6>
+                               </div>
+                               <div class="col-sm-9 ">
+                                   {{ Auth::user()->last_name }}
+                               </div>
+                          </div>
+                          <hr>
+                          <div class="row">
+                             <div class="col-sm-3">
+                                 <h6 class="mb-0"><strong>City</strong></h6>
+                             </div>
+                             <div class="col-sm-9 ">
+                                 {{ Auth::user()->city }}
+                             </div>
+                         </div>
+                         <hr>
+                         <div class="row">
+                              <div class="col-sm-3">
+                                  <h6 class="mb-0"><strong>Phone</strong></h6>
+                              </div>
+                              <div class="col-sm-9 ">
+                                  {{ Auth::user()->phone }}
+                             </div>
+                          </div>
+                          <hr>
+                          <div class="row">
+                             <div class="col-sm-3 ">
+                                  <h6 class="mb-0"><strong>Email</strong></h6>
+                             </div>
+                             <div class="col-sm-9">
+                                 {{ Auth::user()->email }}
+                             </div>
+                          </div>
+                       </div>
+                       <div class="tab-pane fade" id="update" role="tabpanel" aria-labelledby="update-tab">
+                          <form>
+                              <div class="form-row">
+                                 <div class="form-group col-md-4">
+                                     <label for="first_name">First Name</label>
+                                     <input type="text" name="first_name" class="form-control" id="first_name" value="{{ Auth::user()->first_name }}">
+                                 </div>
+                                 <div class="form-group col-md-4">
+                                     <label for="last_name">Last Name</label>
+                                     <input type="text" name="last_name" class="form-control" id="last_name" value="{{ Auth::user()->last_name }}">
+                                 </div>
+                                 <div class="form-group col-md-4">
+                                     <label for="username">User Name</label>
+                                     <input type="text" name="username" class="form-control" id="username" value="{{ Auth::user()->username }}">
+                                 </div>
+                              </div>
+                              <div class="form-row">
+                                 <div class="form-group col-md-12">
+                                     <label for="adress">City</label>
+                                     <input type="text" name="city" class="form-control" id="city" value="{{ Auth::user()->city }}">
+                                 </div>
+                              </div>
+                              <div class="form-row">
+                                 <div class="form-group col-md-6">
+                                     <label for="phone">Phone</label>
+                                      <input type="tel" name="phone" class="form-control" id="phone" value="{{ Auth::user()->phone }}">
+                                  </div>  
+                                  <div class="form-group col-md-6">
+                                     <label for="email">Email</label>
+                                      <input type="text" name="email" class="form-control" id="email" value="{{ Auth::user()->email }}">
+                                  </div>                    
+                              </div>
+                              <div class="form-group row mb-0">
+                                 <div class="col-md-8 offset-md-4 text-right">
+                                    <button type="submit" class="btn btn-dark">
+                                        Update Info
+                                   </button>
+                               </div>
+                        </div>
+                          </form>
+                       </div>
+                  </div>                
+             </div>
+          </div>
+     </div>
+  </div>
+</div>
 @endsection
 
-
 @section('title')
-Home
+Personal Info
 @endsection
--- resources/views/dashboard/support.blade.php
+++ resources/views/dashboard/support.blade.php
@@ -1,10 +1,43 @@
 @extends('layouts.dashboard')
 
 @section('content')
-            <h1>Hello</h1>
+<div class="container">
+   <div class="card m-5">
+       <div class="card-header">
+          Contact us
+       </div>
+       <div class="card-body">
+          <div class="">
+              <div class="form-row">
+                <div class="col-md-4 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                  <div class="validate"></div>
+                </div>
+                <div class="col-md-4 form-group">
+                  <input type="text" name="phone" class="form-control" id="phone" placeholder="Phone Number" data-rule="minlen:8" data-msg="Please enter your realphone number" />
+                  <div class="validate"></div>
+                </div>
+                <div class="col-md-4 form-group">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Email" data-rule="email" data-msg="Please enter a valid email" />
+                  <div class="validate"></div>
+                </div>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                <div class="validate"></div>
+              </div>
+              <div class="form-group">
+                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                <div class="validate"></div>
+              </div>
+              <div class="text-right"><button  class="btn btn-dark" type="submit">Send</button></div>
+          </div>
+       </div>
+   </div>
+</div>
 @endsection
 
 
 @section('title')
-Home
+Support
 @endsection
