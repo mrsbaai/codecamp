8b6d9c67f2c285bc7efb2b7782a23e8638668fe0
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-23 14:57:08 +0000

    Update settings.blade.php

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -8,7 +8,7 @@
          <div class="card">
             <div class="card-header">{{ __('Personal Info') }}</div>
               <div class="card-body">
-                <form method="POST" action="{{ route('personal') }}">
+                <form method="POST" action="">
                     @csrf
 
                     <div class="form-group row ">
