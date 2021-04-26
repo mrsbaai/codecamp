dc38d35020b5fb9ea5ac23ebcd3486d2bbe55d2c
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-26 21:08:10 +0000

    Update coworkers.blade.php

--- resources/views/dashboard/coworkers.blade.php
+++ resources/views/dashboard/coworkers.blade.php
@@ -2,11 +2,19 @@
 @extends('layouts.dashboard')
 
 @section('content')
+<style>
+   .thumb-icon {
+     color: #CACACA;
+   }
+ </style>
+ 
 <div class="container">
  <div class="row ">
         <div class="col-lg-3 mb-3">
          <div class="card">
+            <div class="thumb-container">
             <img src="https://i.imgur.com/i4uauqV.jpg" class="card-img-top" alt="Abdelilah: Full stack developer">
+            </div>
             <div class="card-body">
                     <h5 class="card-title">Abdelilah</h5>
               <h6 class="card-subtitle mb-2 text-muted">Full stack developer</h6>
@@ -25,131 +33,6 @@
           </div>          
         </div>
 
-        <div class="col-lg-3 mb-3">
-         <div class="card">
-            <img src="https://i.imgur.com/i4uauqV.jpg" class="card-img-top" alt="Abdelilah: Full stack developer">
-            <div class="card-body">
-                    <h5 class="card-title">Abdelilah</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Full stack developer</h6>
-              <p class="card-text">
-                  <span class="badge badge-warning">PHP</span>
-                  <span class="badge badge-warning">Javascript</span>
-                  <span class="badge badge-warning">Bootsrap</span>
-                  <span class="badge badge-warning">CSS</span>
-                  <span class="badge badge-warning">HTML</span>
-                  <span class="badge badge-warning">NodeJS</span>
-                  <span class="badge badge-warning">Mysql</span>
-                  <span class="badge badge-warning">CPA</span>
-                  <span class="badge badge-warning">Facebook Ads</span>
-              </p>
-            </div>
-          </div>          
-        </div>
-
-        <div class="col-lg-3 mb-3">
-         <div class="card">
-            <img src="https://i.imgur.com/i4uauqV.jpg" class="card-img-top" alt="Abdelilah: Full stack developer">
-            <div class="card-body">
-                    <h5 class="card-title">Abdelilah</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Full stack developer</h6>
-              <p class="card-text">
-                  <span class="badge badge-warning">PHP</span>
-                  <span class="badge badge-warning">Javascript</span>
-                  <span class="badge badge-warning">Bootsrap</span>
-                  <span class="badge badge-warning">CSS</span>
-                  <span class="badge badge-warning">HTML</span>
-                  <span class="badge badge-warning">NodeJS</span>
-                  <span class="badge badge-warning">Mysql</span>
-                  <span class="badge badge-warning">CPA</span>
-                  <span class="badge badge-warning">Facebook Ads</span>
-              </p>
-            </div>
-          </div>          
-        </div>
-
-        <div class="col-lg-3 mb-3">
-         <div class="card">
-            <img src="https://i.imgur.com/i4uauqV.jpg" class="card-img-top" alt="Abdelilah: Full stack developer">
-            <div class="card-body">
-                    <h5 class="card-title">Abdelilah</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Full stack developer</h6>
-              <p class="card-text">
-                  <span class="badge badge-warning">PHP</span>
-                  <span class="badge badge-warning">Javascript</span>
-                  <span class="badge badge-warning">Bootsrap</span>
-                  <span class="badge badge-warning">CSS</span>
-                  <span class="badge badge-warning">HTML</span>
-                  <span class="badge badge-warning">NodeJS</span>
-                  <span class="badge badge-warning">Mysql</span>
-                  <span class="badge badge-warning">CPA</span>
-                  <span class="badge badge-warning">Facebook Ads</span>
-              </p>
-            </div>
-          </div>          
-        </div>
-
-        <div class="col-lg-3 mb-3">
-         <div class="card">
-            <img src="https://i.imgur.com/i4uauqV.jpg" class="card-img-top" alt="Abdelilah: Full stack developer">
-            <div class="card-body">
-                    <h5 class="card-title">Abdelilah</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Full stack developer</h6>
-              <p class="card-text">
-                  <span class="badge badge-warning">PHP</span>
-                  <span class="badge badge-warning">Javascript</span>
-                  <span class="badge badge-warning">Bootsrap</span>
-                  <span class="badge badge-warning">CSS</span>
-                  <span class="badge badge-warning">HTML</span>
-                  <span class="badge badge-warning">NodeJS</span>
-                  <span class="badge badge-warning">Mysql</span>
-                  <span class="badge badge-warning">CPA</span>
-                  <span class="badge badge-warning">Facebook Ads</span>
-              </p>
-            </div>
-          </div>          
-        </div>
-
-        <div class="col-lg-3 mb-3">
-         <div class="card">
-            <img src="https://i.imgur.com/i4uauqV.jpg" class="card-img-top" alt="Abdelilah: Full stack developer">
-            <div class="card-body">
-                    <h5 class="card-title">Abdelilah</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Full stack developer</h6>
-              <p class="card-text">
-                  <span class="badge badge-warning">PHP</span>
-                  <span class="badge badge-warning">Javascript</span>
-                  <span class="badge badge-warning">Bootsrap</span>
-                  <span class="badge badge-warning">CSS</span>
-                  <span class="badge badge-warning">HTML</span>
-                  <span class="badge badge-warning">NodeJS</span>
-                  <span class="badge badge-warning">Mysql</span>
-                  <span class="badge badge-warning">CPA</span>
-                  <span class="badge badge-warning">Facebook Ads</span>
-              </p>
-            </div>
-          </div>          
-        </div>
-
-        <div class="col-lg-3 mb-3">
-         <div class="card">
-            <img src="https://i.imgur.com/i4uauqV.jpg" class="card-img-top" alt="Abdelilah: Full stack developer">
-            <div class="card-body">
-                    <h5 class="card-title">Abdelilah</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Full stack developer</h6>
-              <p class="card-text">
-                  <span class="badge badge-warning">PHP</span>
-                  <span class="badge badge-warning">Javascript</span>
-                  <span class="badge badge-warning">Bootsrap</span>
-                  <span class="badge badge-warning">CSS</span>
-                  <span class="badge badge-warning">HTML</span>
-                  <span class="badge badge-warning">NodeJS</span>
-                  <span class="badge badge-warning">Mysql</span>
-                  <span class="badge badge-warning">CPA</span>
-                  <span class="badge badge-warning">Facebook Ads</span>
-              </p>
-            </div>
-          </div>          
-        </div>
         
   </div>
   
