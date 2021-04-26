44796fd3a58b407a435910eaf9109626d8f9fa7d
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-26 17:24:09 +0000

    Update settings.blade.php

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -215,32 +215,32 @@
                              <p class="text-danger">{{ $error }}</p>
                           @endforeach 
                          <div class="form-group row">
-                            <div class="col-md-6">
+                       
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
                                     <span class="input-group-text">{{ __('Current Password') }}</span>
                                     </div><input id="password" type="password" class="form-control" name="current_password">
                                 </div>
-                            </div>
+                        
                         </div>
                          <div class="form-group row">
-                            <div class="col-md-6">
+                    
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
                                     <span class="input-group-text">{{ __('New Password') }}</span>
                                     </div><input id="new_password" type="password" class="form-control" name="new_password">
                                 </div>
-                            </div>
+
                          </div>
 
                          <div class="form-group row">
-                            <div class="col-md-6">
+              
                                 <div class="input-group mb-3">
                                     <div class="input-group-prepend">
                                     <span class="input-group-text">{{ __('Confirm Password') }}</span>
                                     </div><input id="confirm_password" type="password" class="form-control" name="confirm_password">
                                 </div>
-                            </div>
+ 
                          </div>
 
 
