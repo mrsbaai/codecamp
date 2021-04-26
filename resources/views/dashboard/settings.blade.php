76780d6b2897cab53734b231ab1610095962d882
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-26 17:27:42 +0000

    Update settings.blade.php

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -216,7 +216,7 @@
                           @endforeach 
                          <div class="form-group row">
                        
-                                <div class="input-group mb-3">
+                                <div class="input-group m-3 mb-1">
                                     <div class="input-group-prepend">
                                     <span class="input-group-text">{{ __('Current Password') }}</span>
                                     </div><input id="password" type="password" class="form-control" name="current_password">
@@ -225,7 +225,7 @@
                         </div>
                          <div class="form-group row">
                     
-                                <div class="input-group m-3">
+                                <div class="input-group m-3 mb-1">
                                     <div class="input-group-prepend">
                                     <span class="input-group-text">{{ __('New Password') }}</span>
                                     </div><input id="new_password" type="password" class="form-control" name="new_password">
@@ -235,7 +235,7 @@
 
                          <div class="form-group row">
               
-                                <div class="input-group m-3">
+                                <div class="input-group m-3 mb-1">
                                     <div class="input-group-prepend">
                                     <span class="input-group-text">{{ __('Confirm Password') }}</span>
                                     </div><input id="confirm_password" type="password" class="form-control" name="confirm_password">
