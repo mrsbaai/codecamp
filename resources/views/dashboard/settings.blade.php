b631182de2a87af9c62cb4d1e4380c422813cea2
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-26 17:02:30 +0000

    Update settings.blade.php

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -41,24 +41,24 @@
 
                     <div class="form-group row ">
                         <div class="col-md-6 my-1">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">{{ __('First Name') }}</div>
+                            <div for="first_name" class="input-group-text">
+                                <div for="first_name" class="input-group-text">{{ __('First Name') }}</div>
                               </div>
                             <input id="first_name" type="text" class="form-control @error('name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
 
-                            @error('name')
+                            @error('first_name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
                         </div>
                          <div class="col-md-6 my-1">
-                            <div class="input-group-prepend">
+                            <div for="last_name" class="input-group-text">
                                 <div class="input-group-text">{{ __('Last Name') }}</div>
                               </div>
                             <input id="last_name" type="text"  class="form-control @error('name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
 
-                            @error('name')
+                            @error('last_name')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
@@ -68,9 +68,10 @@
 
                     <div class="form-group row ">
                         <div class="col-md-6 my-1 ">
-                            <div class="input-group-prepend">
+                            <div for="email" class="input-group-text">
                                 <div class="input-group-text">{{ __('Email') }}</div>
                               </div>
+                              <input id="email" type="email" class="form-control " name="email" required autocomplete="email">
                             
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -79,13 +80,13 @@
                             @enderror
                         </div>
                         <div class="col-md-6 my-1">
-                            <div class="input-group-prepend">
+                            <div for="phone" class="input-group-text">
                                 <div class="input-group-text">{{ __('Phone') }}</div>
                               </div>
                             
                             <input id="phone" type="tel" class="form-control @error('') is-invalid @enderror" name="phone" value="{{ old('phone') }}" required autocomplete="phone" autofocus>
 
-                            @error('name')
+                            @error('phone')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
@@ -95,7 +96,7 @@
 
                     <div class="form-group row mb-3">
                         <div class="col-md-12">
-                            <div class="input-group-prepend">
+                            <div for="city" class="input-group-text">
                                 <div class="input-group-text">{{ __('I currently live in') }}:</div>
                               </div>
                             <select class="custom-select" id="city" name="city" data-rule="required" required="required" data-msg="Please select your current city" >                    
@@ -167,7 +168,7 @@
                                  <option value="Azrou">Azrou</option>
                                 
                           </select>
-                            @error('name')
+                            @error('city')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
@@ -202,22 +203,27 @@
                              <p class="text-danger">{{ $error }}</p>
                           @endforeach 
                          <div class="form-group row">
-                             <label for="password" class="col-md-4 col-form-label text-md-right">Current Password</label>
+                            <div for="password" class="input-group-text">
+                                <div class="input-group-text">{{ __('Current Password') }}:</div>
+                              </div>
                              <div class="col-md-6">
-                                 <input id="password" type="password" class="form-control" name="current_password" autocomplete="current-password">
+                                 <input id="password" type="password" class="form-control" name="current_password">
                              </div>
                          </div>
                          <div class="form-group row">
-                             <label for="password" class="col-md-4 col-form-label text-md-right">New Password</label>
+                            <div for="new_password" class="input-group-text">
+                                <div class="input-group-text">{{ __('New Password') }}:</div>
+                              </div>
                              <div class="col-md-6">
-                                 <input id="new_password" type="password" class="form-control" name="new_password" autocomplete="current-password">
+                                 <input id="new_password" type="password" class="form-control" name="new_password">
                              </div>
                          </div>
                          <div class="form-group row">
-                             <label for="password" class="col-md-4 col-form-label text-md-right">New Confirm Password</label>
+                            <div for="confirm_password" class="input-group-text">
+                                <div class="input-group-text">{{ __('Confirm New Password') }}:</div>
+                              </div>
                              <div class="col-md-6">
- 
-                                 <input id="new_confirm_password" type="password" class="form-control" name="new_confirm_password" autocomplete="current-password">
+                                 <input id="confirm_password" type="password" class="form-control" name="confirm_password">
                              </div>
                          </div>
                          <div class="form-group row mb-0">
