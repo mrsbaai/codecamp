b718d262722eb36e3dafe4b1f0af224555947713
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-27 00:28:31 +0000

    Update messages.blade.php

--- resources/views/dashboard/messages.blade.php
+++ resources/views/dashboard/messages.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-lg-12">
+        <div class="col-lg-12 pt-4">
         <div class="card">
              <div class="card-header ">
                 Inbound Messages 
