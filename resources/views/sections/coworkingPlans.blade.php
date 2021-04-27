60955e3b5ebc34e9092b78c313e3b63ce51c1cf6
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-27 00:39:45 +0000

    Update coworkingPlans.blade.php

--- resources/views/sections/coworkingPlans.blade.php
+++ resources/views/sections/coworkingPlans.blade.php
@@ -1,5 +1,44 @@
 @section('plans')
+<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+    <h1 class="display-4">Pricing</h1>
+    <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It's built with default Bootstrap components and utilities with little customization.</p>
+</div>
 
-plan 1 / plan 2
+<div class="container">
+    <div class="card-deck mb-3 text-center">
+      <div class="card mb-4 box-shadow">
+        <div class="card-header">
+          <h4 class="my-0 font-weight-normal">Monthly</h4>
+        </div>
+        <div class="card-body">
+          <h1 class="card-title pricing-card-title">600dh <small class="text-muted">/ Mo</small></h1>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>10 users included</li>
+            <li>2 GB of storage</li>
+            <li>Email support</li>
+            <li>Help center access</li>
+          </ul>
+          <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
+        </div>
+      </div>
+      <div class="card mb-4 box-shadow">
+        <div class="card-header">
+          <h4 class="my-0 font-weight-normal">Yearly</h4>
+        </div>
+        <div class="card-body">
+          <h1 class="card-title pricing-card-title">4800dh <small class="text-muted">/ Yr</small></h1>
+          <ul class="list-unstyled mt-3 mb-4">
+            <li>20 users included</li>
+            <li>10 GB of storage</li>
+            <li>Priority email support</li>
+            <li>Help center access</li>
+          </ul>
+          <button type="button" class="btn btn-lg btn-block btn-primary">Get started</button>
+        </div>
+      </div>
+      
+    </div>
 
+    
+  </div>
 @endsection
