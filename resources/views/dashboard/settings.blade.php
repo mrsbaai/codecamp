fb125ff85e83b078bd72c24bbb91f0e568e11c72
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-27 00:19:37 +0000

    Update settings.blade.php

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -248,6 +248,20 @@
   
   </div>
 
+
+  <div class="container">
+
+    <div class="row">
+      <div class="col-xl-8">
+        1 of 3
+      </div>
+      <div class="col-xl-4">
+        2 of 3 (wider)
+      </div>
+
+    </div>
+  </div>
+
 @endsection
 
 @section('title')
