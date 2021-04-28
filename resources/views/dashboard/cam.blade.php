a84a5df85d37382fe242bcfba17eab6d43f78913
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-28 22:37:11 +0000

    Update cam.blade.php

--- resources/views/dashboard/cam.blade.php
+++ resources/views/dashboard/cam.blade.php
@@ -8,19 +8,19 @@
 </div>
 
 <div class="row justify-content-center">
-    <div class="col-lg-5 mb-4 text-center">
+    <div class="col-lg-4 mb-4 text-center">
         <iframe width="375" height="310" src="https://rtsp.me/embed/RRiH8zAF/" frameborder="0"></iframe> 
     </div>
-    <div class="col-lg-5 mb-4 text-center">
+    <div class="col-lg-4 mb-4 text-center">
         <iframe width="375" height="310" src="https://rtsp.me/embed/y3dHDeke/" frameborder="0"></iframe>
     </div>
 </div>
 
 <div class="row justify-content-center">
-    <div class="col-lg-5 mb-4 text-center">
+    <div class="col-lg-4 mb-4 text-center">
         <iframe width="375" height="310" src="https://rtsp.me/embed/ibDNddKz/" frameborder="0"></iframe>      
     </div>
-    <div class="col-lg-5 mb-4 text-center">
+    <div class="col-lg-4 mb-4 text-center">
         <iframe width="375" height="310" src="https://rtsp.me/embed/EnQ5ehkK/" frameborder="0"></iframe>
     </div>
 </div>
