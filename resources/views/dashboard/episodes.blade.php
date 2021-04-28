fdfed3ee1b37d77f0c05522d0246d49c527e1196
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-28 23:34:32 +0000

    up

--- resources/views/dashboard/episodes.blade.php
+++ resources/views/dashboard/episodes.blade.php
@@ -1,8 +1,127 @@
 @extends('layouts.dashboard')
-@include('sections.episodes')
+
 
 @section('content')
-@yield('episodes')
+
+<div class="container mb-5">
+  
+    <div class="row">
+   
+      <div class="col-lg-7 pb-5">
+    
+        <div class="card" >
+    
+          <div class="card-body p-0 mb-5">
+           <div class="embed-responsive embed-responsive-16by9 mb-4" style="border-top-right-radius: calc(0.25rem - 1px);border-top-left-radius: calc(0.25rem - 1px);">
+             <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/ZA0eyrP15iI" allowfullscreen></iframe>
+           </div>
+           <div class="m-3">
+             <h3 class="card-title">2021 New Year Online Business Resolution</h3>
+             
+             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+             <h5>Guests</h5>
+             <hr>
+             <p><a href="#">ALI</a></p>
+             <p><a href="#">ANASS</a></p>
+             <p><a href="#">ADAM</a></p>
+             <h5>Notes</h5>
+             <hr>
+             <p><a href="#">1:56</a> - Advice about Performance Optimization.</p>
+             <p><a href="#">1:56</a> - Advice about Performance Optimization.</p>
+             <p><a href="#">1:56</a> - Advice about Performance Optimization.</p>
+             <p><a href="#">1:56</a> - Advice about Performance Optimization.</p>
+             <p><a href="#">1:56</a> - Advice about Performance Optimization.</p>
+             <p><a href="#">1:56</a> - Advice about Performance Optimization.</p>
+             <h5>Links</h5>
+             <hr>
+             <p><a href="#">Advice about Performance Optimization</a></p>
+             <p><a href="#">Advice about Performance Optimization</a></p>
+             <h5>Prepared and Presented by</h5>
+             <hr>
+             <p><a href="#">ALI</a></p>
+             <p><a href="#">ANASS</a></p>
+             <p><a href="#">ADAM</a></p>
+             </div>
+           
+           </div>
+         </div>
+    
+      </div>
+    
+      <div class="col-lg-5">
+
+        <select class="custom-select" id="category" name="category" >
+          <option value="All">All Episodes</option>
+          <option value="MSS">Moroccan Success Stories</option>
+          <option value="MC">Modern Careers</option>
+          <option value="TECH">Technology</option>
+        </select>
+
+       <div class="list-group pt-4">
+    
+         <a href="#" class="list-group-item list-group-item-action ">
+           <div class="d-flex w-100 justify-content-between">
+             <div class="col-2 p-1">
+               <div class="text-left pt-3">
+                  <h1><i class="fas fa-play"></i> </h1>
+                </div>
+             </div>
+             <div class="col-10 p-1">
+                <div class="row">
+                  <h6>2021 New Year Online Business Resolution Resolution Resolution</h6>
+                </div>
+                <div class="row">
+                   <p class="card-text"><small class="text-muted">02:12:38 | 01/03/2021</small></p>
+                 </div>
+              </div>
+           </div>   
+          </a>
+    
+          <a href="#" class="list-group-item list-group-item-action ">
+            <div class="d-flex w-100 justify-content-between">
+              <div class="col-2 p-1">
+                <div class="text-left pt-3">
+                   <h1><i class="fas fa-play"></i> </h1>
+                 </div>
+              </div>
+              <div class="col-10 p-1">
+                 <div class="row">
+                   <h6>2021 New Year Online Business Resolution</h6>
+                 </div>
+                 <div class="row">
+                    <p class="card-text"><small class="text-muted">02:12:38 | 01/03/2021</small></p>
+                  </div>
+               </div>
+            </div>   
+           </a>
+    
+           <a href="#" class="list-group-item list-group-item-action ">
+            <div class="d-flex w-100 justify-content-between">
+              <div class="col-2 p-1">
+                <div class="text-left pt-3">
+                   <h1><i class="fas fa-play"></i> </h1>
+                 </div>
+              </div>
+              <div class="col-10 p-1">
+                 <div class="row">
+                   <h6>2021 New Year Online Business Resolution</h6>
+                 </div>
+                 <div class="row">
+                    <p class="card-text"><small class="text-muted">02:12:38 | 01/03/2021</small></p>
+                  </div>
+               </div>
+            </div>   
+           </a>
+    
+     
+       </div>
+     </div>
+    </div>
+             
+    
+        
+    </div>
+    
 @endsection
 
 
