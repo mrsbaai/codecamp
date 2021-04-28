1b11ffdfaf3e20a914904fd1e6800a875d7b4b2e
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-28 22:42:31 +0000

    Update support.blade.php

--- resources/views/dashboard/support.blade.php
+++ resources/views/dashboard/support.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="container vh-100">
+<div class="container vh-100 tect-center">
+  <h1 class="display-4">How Can We Help?</h1>
    <div class="card m-5">
-       <div class="card-header">
-          How can we help?
-       </div>
-       <div class="card-body">
-          <div class="">
+
+       <div class="card-body bg-dark text-light">
+   
             
             <div class="form-row">
               <div class="col-12 form-group">
-              <p>You have any questions or concerns about CodeCamp? You have a suggestion? Please let us kown.</p>
+              
               </p>
             </div>
               <div class="form-row">
@@ -36,9 +35,9 @@
                 <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
                 <div class="validate"></div>
               </div>
-              <div class="text-right"><button  class="btn btn-dark" type="submit">Send</button></div>
+              <div class="text-right"><button  class="btn btn-lg btn-primary" type="submit">Send</button></div>
           </div>
-       </div>
+
    </div>
   </div>
 </div>
