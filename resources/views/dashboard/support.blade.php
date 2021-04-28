d5d46c93725806c418c1c1c0f0ae33819ec3c73c
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-28 22:48:59 +0000

    Update support.blade.php

--- resources/views/dashboard/support.blade.php
+++ resources/views/dashboard/support.blade.php
@@ -1,19 +1,16 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="container vh-100 tect-center">
+<div class="container vh-100 text-center">
   <h1 class="display-4">How Can We Help?</h1>
    <div class="card m-5">
 
        <div class="card-body bg-dark text-light">
    
             
-            <div class="form-row">
-              <div class="col-12 form-group">
-              
-              </p>
-            </div>
+        
               <div class="form-row">
+                
                 <div class="col-md-4 form-group">
                   <input type="text" name="name" class="form-control" id="name" placeholder="Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                   <div class="validate"></div>
@@ -27,16 +24,23 @@
                   <div class="validate"></div>
                 </div>
               </div>
-              <div class="form-group">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                <div class="validate"></div>
+
+              <div class="form-row">
+                <div class="form-group">
+                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                  <div class="validate"></div>
+                </div>
               </div>
-              <div class="form-group">
-                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                <div class="validate"></div>
+
+              <div class="form-row">
+                <div class="form-group">
+                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                  <div class="validate"></div>
+                </div>
               </div>
-              <div class="text-right"><button  class="btn btn-lg btn-primary" type="submit">Send</button></div>
-          </div>
+              <div class="form-row">
+                <div class="text-right"><button  class="btn btn-lg btn-primary" type="submit">Send</button></div>
+              </div>
 
    </div>
   </div>
