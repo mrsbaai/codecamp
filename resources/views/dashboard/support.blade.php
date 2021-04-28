d439dd2418cb83133e058a03b24900cc725d8973
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-28 22:52:06 +0000

    Update support.blade.php

--- resources/views/dashboard/support.blade.php
+++ resources/views/dashboard/support.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="container vh-100 text-center">
+<div class="container vh-100 text-center pt-4">
   <h1 class="display-4">How Can We Help?</h1>
    <div class="card m-5">
 
@@ -26,20 +26,26 @@
               </div>
 
               <div class="form-row">
-                <div class="form-group">
-                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                  <div class="validate"></div>
+                <div class="col-12 form-group">
+                  <div class="form-group">
+                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                    <div class="validate"></div>
+                  </div>
                 </div>
               </div>
 
               <div class="form-row">
-                <div class="form-group">
-                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                  <div class="validate"></div>
+                <div class="col-12 form-group">
+                  <div class="form-group">
+                    <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                    <div class="validate"></div>
+                  </div>
                 </div>
               </div>
               <div class="form-row">
-                <div class="text-right"><button  class="btn btn-lg btn-primary" type="submit">Send</button></div>
+                <div class="col-12 form-group">
+                  <div class="text-right"><button  class="btn btn-lg btn-primary" type="submit">Send</button></div>
+                </div>
               </div>
 
    </div>
