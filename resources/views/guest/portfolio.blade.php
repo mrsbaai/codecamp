5f2737bcff8e545e120b86ff6c4ccd801390f0a1
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-29 03:27:55 +0000

    up

--- resources/views/guest/portfolio.blade.php
+++ resources/views/guest/portfolio.blade.php
@@ -54,7 +54,7 @@
         <header class="masthead bg-primary text-white text-center">
             <div class="container d-flex align-items-center flex-column">
                 <!-- Masthead Avatar Image-->
-                <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="..." />
+                <img src="https://i.imgur.com/KmeTFmn.jpg" class="masthead-avatar mb-5" alt="Full Stack Developer Tetouan">
                 <!-- Masthead Heading-->
                 <h1 class="masthead-heading text-uppercase mb-0">Hello! I'm Abdelilah</h1>
                 <!-- Icon Divider-->
