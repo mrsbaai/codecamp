cb57b5dbf449cd42467c653d0a182c4a8591d2de
mrsbaai <abdelilah.sbaai@gmail.com>
2021-04-30 05:20:54 +0000

    up

--- resources/views/guest/portfolio.blade.php
+++ resources/views/guest/portfolio.blade.php
@@ -6,22 +6,23 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Abdelilah Sbaai: Full Stack Developper</title>
+        
 
         <!-- Favicons -->
-        <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('img/favicon/apple-icon-57x57.png') }}">
-        <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('img/favicon/apple-icon-60x60.png') }}">
-        <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('img/favicon/apple-icon-72x72.png') }}">
-        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/favicon/apple-icon-76x76.png') }}">
-        <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('img/favicon/apple-icon-114x114.png') }}">
-        <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('img/favicon/apple-icon-120x120.png') }}">
-        <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('img/favicon/apple-icon-144x144.png') }}">
-        <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('img/favicon/apple-icon-152x152.png') }}">
-        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicon/apple-icon-180x180.png') }}">
-        <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('img/favicon/android-icon-192x192.png') }}">
-        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
-        <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('img/favicon/favicon-96x96.png') }}">
-        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png') }}">
-        <link rel="manifest" href="{{ asset('img/favicon/manifest.json') }}">
+        <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('img/portfolio-favicon/apple-icon-57x57.png') }}">
+        <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('img/portfolio-favicon/apple-icon-60x60.png') }}">
+        <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('img/portfolio-favicon/apple-icon-72x72.png') }}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/portfolio-favicon/apple-icon-76x76.png') }}">
+        <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('img/portfolio-favicon/apple-icon-114x114.png') }}">
+        <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('img/portfolio-favicon/apple-icon-120x120.png') }}">
+        <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('img/portfolio-favicon/apple-icon-144x144.png') }}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('img/portfolio-favicon/apple-icon-152x152.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/portfolio-favicon/apple-icon-180x180.png') }}">
+        <link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('img/portfolio-favicon/android-icon-192x192.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/portfolio-favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('img/portfolio-favicon/favicon-96x96.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/portfolio-favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('img/portfolio-favicon/manifest.json') }}">
     
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
@@ -95,7 +96,6 @@
                     <span class="badge badge-primary p-2 m-2 "><p class="display-4 m-1">Fat-Free Framework</p></span>
                     <span class="badge badge-primary p-2 m-2 "><p class="display-4 m-1">Apache</p></span>
                     <span class="badge badge-primary p-2 m-2 "><p class="display-4 m-1">Linux</p></span>
-                    <span class="badge badge-primary p-2 m-2 "><p class="display-4 m-1">L</p></span>
                     <span class="badge badge-primary p-2 m-2 "><p class="display-4 m-1">Nginx</p></span>
                     <span class="badge badge-primary p-2 m-2 "><p class="display-4 m-1">PHP</p></span>
                     <span class="badge badge-primary p-2 m-2 "><p class="display-4 m-1">MongoDB</p></span>
