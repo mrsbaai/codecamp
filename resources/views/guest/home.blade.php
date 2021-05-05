fdd5053bbf60f36a63d435aa6e6fc68942b50fe3
mrsbaai <abdelilah.sbaai@gmail.com>
2021-05-05 19:39:37 +0000

    up

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -280,6 +280,31 @@
 
 @section('bottom')
 <script src="{{ asset('js/typed.js/typed.min.js') }}" ></script>
+<script>
+    /**
+ * Template Name: MyResume - v2.1.0
+ * Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+!(function($) {
+    "use strict";
+
+    // Hero typed
+    if ($('.typed').length) {
+        var typed_strings = $(".typed").data('typed-items');
+        typed_strings = typed_strings.split(',')
+        new Typed('.typed', {
+            strings: typed_strings,
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
+
+})(jQuery);
+</script>
 @endsection
 
 @section('title')
