45c4f57df43ea32380dca2da6a66ac8eae44b6d1
mrsbaai <abdelilah.sbaai@gmail.com>
2021-05-15 08:55:12 +0000

    Update edit-portfolio.js

--- public/js/edit-portfolio.js
+++ public/js/edit-portfolio.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
-    var actions = $("table td:first-child").html();
+    var actions = $("table td:last-child").html();
     // Append table with add row form on add new button click
     $(".add-new").click(function() {
         var index = $("table tbody tr:first-child").index();
@@ -9,10 +9,13 @@ $(document).ready(function() {
             '<td><textarea type="text" class="form-control" name="description" id="description" placeholder="Decription (Ex Moroccan web experts community. An Initiative to highlight porductivity tools for entroponeurs, moroccan success stories, modern carer paths and informational technologies.)" ></textarea></td>' +
             '<td><input type="text" class="form-control" name="link" id="link" placeholder="Link (Ex https://www.codecamp.ma or FB.com/CodeCamp.ma)"  ></td>' +
             '<td><input type="text" class="form-control" name="release" id="release" placeholder="Release Year (Ex 2020)" ></td>' +
-            '<td>' + actions + '</td>' +
+            '<td>' +
+            '<a class="add" title="Add" data-toggle="tooltip"><i class="fa fa-thumbs-up"></i></a>' +
+            '<a class="delete" title="Delete" data-toggle="tooltip"><i class="fa fa-trash"></i></a>' +
+            '</td>' +
             '</tr>';
         $("table").prepend(row);
-        $("table tbody tr").eq(index - 1).find(".add").toggle();
+        // $("table tbody tr").eq(index + 1).find(".add").toggle();
         $('[data-toggle="tooltip"]').tooltip();
     });
     // Add row on add button click
