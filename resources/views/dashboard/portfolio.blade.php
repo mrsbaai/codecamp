b2de0d850fbc83036f0a06a3df20fb4cef560fdc
mrsbaai <abdelilah.sbaai@gmail.com>
2021-05-17 10:53:04 +0100

    Update portfolio.blade.php

--- resources/views/dashboard/portfolio.blade.php
+++ resources/views/dashboard/portfolio.blade.php
@@ -336,6 +336,15 @@
       }
    }
 
+$(document).ready(function() {
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+});
+
 </script>
 @endsection
 
