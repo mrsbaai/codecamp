526165d5966790514b887c640ffb2aa6ae697245
mrsbaai <abdelilah.sbaai@gmail.com>
2021-05-18 12:26:46 +0100

    Update portfolio.blade.php

--- resources/views/guest/portfolio.blade.php
+++ resources/views/guest/portfolio.blade.php
@@ -305,50 +305,18 @@
         <!-- Footer-->
         <footer class="footer text-center">
             <div class="container">
-                <div class="row">
-                    <!-- Footer Location-->
-                    <div class="col-lg-4 mb-5 mb-lg-0">
-                        <h4 class="text-uppercase mb-4">Location</h4>
-                        <p class="lead mb-0">
-                            2215 John Daniel Drive
-                            <br />
-                            Clark, MO 65243
-                        </p>
-                    </div>
-                    <!-- Footer Social Icons-->
-                    <div class="col-lg-4 mb-5 mb-lg-0">
-                        <h4 class="text-uppercase mb-4">Around the Web</h4>
-                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
-                    </div>
-                    <!-- Footer About Text-->
-                    <div class="col-lg-4">
-                        <h4 class="text-uppercase mb-4">About Freelancer</h4>
-                        <p class="lead mb-0">
-                            Freelance is a free to use, MIT licensed Bootstrap theme created by
-                            <a href="http://startbootstrap.com">Start Bootstrap</a>
-                            .
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!-- Copyright Section-->
-        <div class="copyright py-4 text-center text-white">
-            <div class="container">
-                <small>
-                    Made With&nbsp;&nbsp;<span style="color: #FF1616;"><i class="fas fa-heart"></i></span>&nbsp;&nbsp;Using <a href="https://codecamp.ma">CodeCamp.ma</a>
+                <p class="lead mb-0">
+                    Made With&nbsp;&nbsp;<span style="color: #F60109;"><i class="fas fa-heart"></i></span>&nbsp;&nbsp;Using <a style="font-weight:800px; color: #F60109; :hover {color: #FF1616}" href="https://codecamp.ma">CodeCamp.ma</a>
                     Copyright &copy;
                     <!-- This script automatically adds the current year to your website footer-->
                     <!-- (credit: https://updateyourfooter.com/)-->
                     <script>
                         document.write(new Date().getFullYear());
                     </script>
-                </small>
+                </p>
             </div>
-        </div>
+        </footer>
+
         <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes)-->
         <div class="scroll-to-top d-lg-none position-fixed">
             <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top"><i class="fa fa-chevron-up"></i></a>
