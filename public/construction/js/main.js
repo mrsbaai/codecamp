b8028ac95bdd3adda4c8c68a3deefcd1df8a6023
mrsbaai <abdelilah.sbaai@gmail.com>
2021-07-11 18:36:17 +0100

    Update main.js

--- public/construction/js/main.js
+++ public/construction/js/main.js
@@ -23,9 +23,9 @@
         new Typed('.typed', {
             strings: typed_strings,
             loop: true,
-            typeSpeed: 100,
-            backSpeed: 50,
-            backDelay: 2000
+            typeSpeed: 80,
+            backSpeed: 20,
+            backDelay: 1500
         });
     }
 
