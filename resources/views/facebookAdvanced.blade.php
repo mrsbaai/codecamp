615ff0bf4cf04c6908e3efb0e93c3fdd52f8a7bd
mrsbaai <abdelilah.sbaai@gmail.com>
2021-07-13 17:19:37 +0100

    Update facebookAdvanced.blade.php

--- resources/views/facebookAdvanced.blade.php
+++ resources/views/facebookAdvanced.blade.php
@@ -426,7 +426,7 @@
                (Advanced Training)</b>
                <br/><br/>Sun, Aug 1st - <b>TETOUAN</b>
             <br/><br/>
-            <b><span class="typed" data-typed-items="Want To Become A Facebook Ads Expert?, This Training is A Required Skill For Anyone With..., A Business, A Product, A Service, A Brand, Or A Public Figure, If You Want To PROMOTE/SCALE Your Business, To 6 Figures And More!"></span></b>
+            <span class="typed" data-typed-items="Want To Become A Facebook Ads Expert?, This Training is A Required Skill For Anyone With..., A Business, A Product, A Service, A Brand, Or A Public Figure, If You Want To PROMOTE/SCALE Your Business, To 6 Figures And More!"></span>
             <br/>
             
           </h2>
