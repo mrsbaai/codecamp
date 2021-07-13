5531eaf88aab4740b714eed0fc2a5bd8fe2f4d57
mrsbaai <abdelilah.sbaai@gmail.com>
2021-07-13 12:57:54 +0100

    Update facebookAdvanced.blade.php

--- resources/views/facebookAdvanced.blade.php
+++ resources/views/facebookAdvanced.blade.php
@@ -448,9 +448,9 @@
     <div class="container" data-aos="fade-up">
       <div class="row">
         <div class="col-md-12">
-          <br/> <br/> <br/>
+          <br/> 
           <div class="img-fluid text-white text-center">
-            <img alt="wail Amrani" class="mw-100" src="https://codecamp.ma/img/wail-en.png" width="500" height="auto">
+            <img alt="wail Amrani" class="mw-100 pb-5" src="https://codecamp.ma/img/wail-en.png" width="500" height="auto">
             <h1>"Take a step in the right direction with your life and business… Learn how Facebook ads will make your businesses a household name!"</h1><br/>
          </div>
          <br/> <br/> <br/>
